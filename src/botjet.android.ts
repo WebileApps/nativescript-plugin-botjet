0146a20515c83fcc87205e319b93fb1723cde46c
Rajiv Narayana <rajiv@webileapps.com>
2019-07-06 17:56:05 +0530

    Android sources for the plugin.

--- src/botjet.android.ts
+++ src/botjet.android.ts
@@ -1,8 +1,40 @@
+import * as utils from "tns-core-modules/utils/utils";
 import { BotJetSDKCommon } from './botjet.common';
 import { BotJetConfig } from ".";
 
+declare const ai : any;
+
 export class BotJetSDK extends BotJetSDKCommon {
     public static launchChatWithConfig(config: BotJetConfig) : void {
+        let options = new ai.botjet.android.nativesdk.model.BotConfig();
+        options.setBotId(config.botId);
+        options.setClientId(config.clientId);
         
+        if (config.accentColor) {
+            options.setAccentColor("#323b8c");
+        }
+        if (config.title) {
+            // options.setTitle(config.title);
+        }
+        if (config.isAnonymous) {
+            options.setAnonymous(config.isAnonymous);
+        }
+        if (config.botIcon) {
+            // options.botIcon = config.botIcon;
+        }
+        options.setHost(config.host || "https://qa.botjet.ai");
+        options.setLocationRequired(config.isLocationRequired || true);
+        if (config.senderId) {
+            options.setSenderId(config.senderId);
+        }
+
+        if (config.profileMeta) { 
+            options.setProfileMeta(config.profileMeta.toString());
+        }
+
+        let intent = new android.content.Intent(utils.ad.getApplicationContext(), ai.botjet.android.nativesdk.activities.ChatActivity.class);
+        intent.putExtra("BOT_CONFIG", options);
+        intent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+        utils.ad.getApplicationContext().startActivity(intent);
     }
 }
