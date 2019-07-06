4c96a26e8ccfbe0fef74ecf5f0da80cc2972ee21
Rajiv Narayana <rajiv@webileapps.com>
2019-07-06 15:45:15 +0530

    demo app

--- demo/app/home/home-page.ts
+++ demo/app/home/home-page.ts
@@ -1,5 +1,3 @@
-import { MyPluginClassName } from 'nativescript-myPluginClassName';
-console.log(new MyPluginClassName().message);
 /*
 In NativeScript, a file with the same name as an XML file is known as
 a code-behind file. The code-behind is a great place to place your view
@@ -7,11 +5,24 @@ logic, and to set up your page’s data binding.
 */
 
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
-
+import { BotJetSDK, BotJetConfig } from "nativescript-plugin-botjet";
 import { HomeViewModel } from "./home-view-model";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
 
     page.bindingContext = new HomeViewModel();
+}
+
+export function onTap() {
+
+    const config : BotJetConfig =  { 
+        clientId : "",
+        botId : "",
+        title : "CallHealth Chat Bot",
+        accentColor : "#323b8c",
+        senderId : ""
+    }
+
+    BotJetSDK.launchChatWithConfig(config);
 }
